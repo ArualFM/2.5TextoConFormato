e173d54785b279951dedc4149dfdb8570b56f757
LauraFernandez <laura.fernandezmol@iesdoctorbalmis.com>
2021-09-30 13:53:59 +0200

    Corrijo los errores

--- TextoConFormato2.5/TextoConFormato2.5/MainWindow.xaml.cs
+++ TextoConFormato2.5/TextoConFormato2.5/MainWindow.xaml.cs
@@ -23,74 +23,62 @@ namespace TextoConFormato2._5
         public MainWindow()
         {
             InitializeComponent();
-           
-        }
 
-
-        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.FontWeight = FontWeights.Bold;
-        }
-
-
-        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.FontStyle = FontStyles.Italic;
-        }
-
-        private void NegritaCheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.FontWeight = FontWeights.Normal;
-        }
-
-        private void CursivaCheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.FontStyle = FontStyles.Normal;
-        }
-
-        private void ColorAzulRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.Foreground = Brushes.Blue;
-        }
-
-        private void ColorAzulRadioButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.Foreground = Brushes.Black;
-        }
-
-        private void ColorRojoRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.Foreground = Brushes.Red;
-        }
-
-        private void ColorRojoRadioButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.Foreground = Brushes.Black;
-        }
-
-        private void ColorVerdeRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.Foreground = Brushes.Green;
-        }
-
-        private void ColorVerdeRadioButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
-            MuestraTextoTextBlock.Foreground = Brushes.Black;
         }
 
         private void TextoUsuarioTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             MuestraTextoTextBlock.Text = TextoUsuarioTextBox.Text;
         }
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+            MuestraTextoTextBlock.FontWeight = FontWeights.Bold;
+        }
+
+
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+            MuestraTextoTextBlock.FontStyle = FontStyles.Italic;
+        }
+
+        private void NegritaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            MuestraTextoTextBlock.FontWeight = FontWeights.Normal;
+        }
+
+        private void CursivaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            MuestraTextoTextBlock.FontStyle = FontStyles.Normal;
+        }
+
+        private void ColorAzulRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+            MuestraTextoTextBlock.Foreground = Brushes.Blue;
+        }
+
+
+
+        private void ColorRojoRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+            MuestraTextoTextBlock.Foreground = Brushes.Red;
+        }
+
+
+
+        private void ColorVerdeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+        
+            MuestraTextoTextBlock.Foreground = Brushes.Green;
+        }
+
+
+
+
     }
 }
